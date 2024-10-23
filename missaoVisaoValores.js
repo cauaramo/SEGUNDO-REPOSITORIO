f9dd86eb1ac3b2ffb62c5eeba2797a05c9421ce7
cauaramo <144915488+cauaramo@users.noreply.github.com>
2024-10-22 22:02:34 -0300

    aula 22/10 2

---
+++ missaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'saiba qual é a misão de nossa empresa!',
+    'Nossa misão é...',
+)
+
+criaCartao (
+    'Visão',
+    'saiba qual é a visão de nossa empresa!',
+    'Nossa vissão é',
+)
+
+criaCartao (
+    'Valores',
+     'Saiba quais são os valores de nossa empresa!',
+     'Nossos valores',
+)
