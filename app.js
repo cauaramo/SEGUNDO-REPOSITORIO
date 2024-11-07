e5773c7ba4f01630613c6615ce447456bef2f601
cauaramo <144915488+cauaramo@users.noreply.github.com>
2024-11-06 22:12:36 -0300

    aula06/11

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 function criaCartao (item, pergunta, resposta){
     console.log(item, pergunta, resposta)
+ //console.log(item, pergunta,resposta)
+
+ let flashcard = documet.getElementById('flashcard');
+ let cartao = document.createElement('article');
+
+ cartao.className = 'cartao';
+
+ cartao.innerHTML = `
+     <div class="cartao__conteudo">
+         <h3>${item}</h3>
+         
+         <div class="cartao__conteudo__pergunta">
+             <p>${pergunta}</p>
+         </div>
+         
+         <div class="cartao__conteudo__resposta">
+             <p>${resposta} </p>
+         </div>
+     </div>
+ `
+
+    let respostaEstaVisivel = false;
+
+    function viraCartao () {
+        respostaEstaVisivel = !variavelEstaVisivel;
+        cartao.classList.toggle('active', viraCartao);  
+    }
+
+    cartao.addEventListener('click',viraCartao)
+
+ flashcard.appendChield(cartao);
+
 }
